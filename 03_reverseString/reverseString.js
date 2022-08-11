7de3719babdf56f06696fd28bff20eb87d827348
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-08-12 00:01:56 +0200

    Complete first 3 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let res = "";
+    for (let i = 0; i < string.length; i++) {
+        res += string[string.length - i - 1];
+    }
+    return res;
 };
 
 // Do not edit below this line
