e801b5a5a0289640b32dd47800448adb7d67546f
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-08-12 13:46:21 +0200

    Add exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+    let res = 0;
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second))
+        return "ERROR"
+    if (first < second) {
+        while (first <= second) {
+            res += first;
+            first++;
+        }
+    }
+    else {
+        while (second <= first) {
+            res += second;
+            second++;
+        }
+    }
+    return res;
 };
+
+console.log(sumAll(10, "90"))
 
 // Do not edit below this line
 module.exports = sumAll;
