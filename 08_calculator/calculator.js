1a02b586e89f0148e0f97a6ba3a40a17a3a80207
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-08-12 14:15:37 +0200

    Add exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let res = 0;
+  for (let i = 0; i < array.length; i++) {
+    res += array[i];
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let res = 1;
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+    res = res * array[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let res = 1;
+  for (let i = 1; i <= num; i++) {
+    res *= i;
+  }
+  return res;
 };
+
+console.log(factorial(10))
 
 // Do not edit below this line
 module.exports = {
