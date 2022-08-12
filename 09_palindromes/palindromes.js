51eb7dc3c323db45c94b61d62ed5a27baa7ac4b6
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-08-12 14:36:05 +0200

    Add exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[.,!]/g, "");
+    string = string.split(' ').join('');
+    string = string.toLowerCase();
+    let res = true;
+    for (let i = 0; i < string.length && res; i++) {
+        if (string[i] !== string[string.length - i - 1])
+            res = false;
+    }
+    return res;
 };
+
+console.log(palindromes('ZZZZ car, a man, a maracaz.'))
 
 // Do not edit below this line
 module.exports = palindromes;
