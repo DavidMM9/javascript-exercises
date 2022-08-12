8866c98a4625791c20b3592b2d1cf96a4153ae6e
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-08-12 14:36:27 +0200

    Add exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let prev = 1;
+    let last = 1;
+    let res = 0;
+    if (num < 0)
+        return "OOPS"
+    if (num === 1 || num === 2 || num === "1" || num === "2")
+        return 1;
+    for (let i = 2; i < num; i++) {
+        res = prev + last;
+        prev = last;
+        last = res;
+    }
+    return res;
 };
+
+console.log(fibonacci("2"))
 
 // Do not edit below this line
 module.exports = fibonacci;
